66b2f889862142d07d511e45131a34e2e50a0652
Soumyawhtjr <93872984+Soumyawhtjr@users.noreply.github.com>
2022-07-16 19:40:18 +0530

    Update Code.js

--- js/Code.js
+++ js/Code.js
@@ -4,9 +4,11 @@ const accessCode3 = "DATABASE";
 
 function clues() {
     
+    push()
     fill("white")
-    textSize(15)
-    text("R E V B A I L A", 100,50)
+    textSize(100)
+    text(`Ma'am I can take ${"\n"} the class at 7`, 100,300)
+    pop()
     fill("lightblue")
     text("Hint: Always changing, not constant !!", 100,70)
 
